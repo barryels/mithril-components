81646d22c30fd532e854f7c02ffd5928d2af50d3
Barry Els <barryels@gmail.com>
2017-07-17 12:45:52 +0200

    adding component exports

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+module.exports = {
+	Button: require('./Button'),
+	Checkbox: require('./Checkbox'),
+	GridColumn: require('./GridColumn'),
+	GridContainer: require('./GridContainer'),
+	GridRow: require('./Button'),
+	ProgressBar: require('./ProgressBar'),
+	Tabs: require('./Tabs'),
+};
